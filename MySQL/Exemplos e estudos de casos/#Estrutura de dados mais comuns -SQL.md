a76faffe5867f8d5c234e311df59280ac1a4c5d8
raizapaula93 <raizapaula93@gmail.com>
2022-02-13 21:42:29 -0300

    Exemplo:DML e DDL

---
+++ MySQL/Exemplos e estudos de casos/#Estrutura de dados mais comuns -SQL.md
@@ -0,0 +1,8 @@
+#Estrutura de dados mais comuns -SQL
+
+Estruturas de dados utilizadas em SQL.
+
+
+##DML
+
+##DDL
---
+++ MySQL/Exemplos e estudos de casos/template__db_tb.sql
@@ -0,0 +1,37 @@
+-- CRIAR DB
+CREATE DATABASE db_ecommerce;
+
+--CRIAR TABELA
+USE db_ecommerce;
+
+CREATE TABLE tb_usuarios (
+id_produto INT NOT NULL AUTO_INCREMENT,
+tipo_produto VARCHAR(255)NOT NULL,
+nome_produto VARCHAR(255)NOT NULL,
+preco DECIMAL(10,2)NOT NULL,
+PRIMARY KEY (id_produto)
+);
+-- RENOMEAR TABELA
+RENAME TABLE tb_usuarios TO tb_produtos;
+-- ADICIONAR COLUNA
+ALTER TABLE tb_produtos ADD foto_produto VARCHAR(255) NULL;
+-- INSERIR VALORES
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Eletronicos", "Camera Tekpix", 200.00, "foto jpg");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Eletronicos", "Fone de ouvido da Mônica", 768.00, "foto jpg");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Eletrodomesticos", "TV Samsumg Led 40"" ", 3799.99,0);
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Eletrodomesticos", "Geladeira Electrolux", 1875.00, "foto jpg");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Móveis de Cozinha", "Mesa de Estudos", 369.00, "foto jpg");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Quarto", "Cama Queensize", 1200.00, "foto jpg");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Cama, Mesa e Banho", "Toalha Senador", 50.00, "foto jpg");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Higiene", "Sabonete Protex", 19.90, "NULL");
+INSERT INTO tb_produtos (tipo_produto, nome_produto, preco, foto_produto)VALUES ("Móveis para a Sala", "Sofá Rei", 3789.00, "foto jpg");
+SELECT * FROM tb_produtos;
+
+--Selecionar(READ)
+SELECT * FROM tb_produtos WHERE preco > 500;
+SELECT * FROM tb_produtos WHERE preco < 500;
+
+--UPDATE(ATUALIZAÇÃO)
+SET SQL_SAFE_UPDATES = 0;
+UPDATE tb_produtos SET foto_produto = "foto_jpg" WHERE foto_produto = "0";
+SELECT * FROM tb_produtos;
